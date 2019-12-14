0e8e95b4ece0198004a2596b7d13c12abaf65cf5
Wizard <benjohn13@protonmail.com>
2019-12-14 14:32:59 -0800

    Create create_spreadsheet.py

---
+++ create_spreadsheet.py
@@ -0,0 +1,6 @@
+# Define create_spreadsheet():
+def create_spreadsheet(title, row_count):
+  print("Creating a spreadsheet called " + title + " with " + str(row_count) + " rows.")
+
+
+create_spreadsheet('Spring Crops', 1000)
