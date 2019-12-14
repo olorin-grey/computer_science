0b1553f24132e31cb06df07d034beb799f8f6f19
Wizard <benjohn13@protonmail.com>
2019-12-14 15:20:59 -0800

    Create repeat_lyrics.py

---
+++ repeat_lyrics.py
@@ -0,0 +1,15 @@
+def repeat_stuff(stuff, num_repeats=10):
+  return stuff*num_repeats
+
+lyrics = repeat_stuff("Row ", 3) + "Your Boat. "
+song = repeat_stuff(lyrics)
+
+print(song)
+
+```
+Row Row Row Your Boat. Row Row Row Your Boat. 
+Row Row Row Your Boat. Row Row Row Your Boat. 
+Row Row Row Your Boat. Row Row Row Your Boat. 
+Row Row Row Your Boat. Row Row Row Your Boat. 
+Row Row Row Your Boat. Row Row Row Your Boat. 
+```
