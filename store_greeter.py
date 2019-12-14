c5030c57a46bc928821a073b339e74eb31e926c7
Wizard <benjohn13@protonmail.com>
2019-12-14 14:12:22 -0800

    Create store_greeter.py

---
+++ store_greeter.py
@@ -0,0 +1,7 @@
+def greet_customer(grocery_store, special_item):
+	print("Welcome to " + grocery_store + ".")
+	print("We're having a sale on " + special_item + ".")
+	print("We look forward to serving you and hope\
+	you have fun shopping!")
+
+greet_customer("Wholefoods", "organic Blueberries")
