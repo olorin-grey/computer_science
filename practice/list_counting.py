f351695e0284c89ba539a2c1edda46ee65ae4865
Wizard <benjohn13@protonmail.com>
2020-01-01 21:46:58 -0800

    Create list_counting.py

---
+++ practice/list_counting.py
@@ -0,0 +1,8 @@
+# If we want to know how many times something appears 
+# in a list, we can use the count function.
+votes = ['Jake', 'Jake', 'Laurie', 'Mike', 'Laurie', 'Mike', 'Laurie', 'Jake', \n
+ 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', \n
+ 'Cassie', 'Dale', ' Johnny', 'Mike', 'Jake', 'Jake', 'Cassie', 'Laurie']
+
+jake_votes = votes.count('Jake')
+print(jake_votes)
