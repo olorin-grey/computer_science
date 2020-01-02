39fb382aa293c582222e7429dd40928bcd5414fa
Wizard <benjohn13@protonmail.com>
2020-01-01 22:06:22 -0800

    Update list_counting.py

--- practice/list_counting.py
+++ practice/list_counting.py
@@ -1,8 +1,9 @@
 # If we want to know how many times something appears 
 # in a list, we can use the count function.
-votes = ['Jake', 'Jake', 'Laurie', 'Mike', 'Laurie', 'Mike', 'Laurie', 'Jake', \n
- 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', \n
- 'Cassie', 'Dale', ' Johnny', 'Mike', 'Jake', 'Jake', 'Cassie', 'Laurie']
+votes = ['Jake', 'Jake', 'Laurie', 'Mike', 'Laurie', 'Mike', 'Laurie', 'Jake', 
+         'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 
+         'Laurie', 'Cassie', 'Dale', ' Johnny', 'Mike', 'Jake', 'Jake', 'Cassie', 
+         'Laurie']
 
 jake_votes = votes.count('Jake')
 print(jake_votes)
